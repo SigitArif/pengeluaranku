499a70c64ee8438c182f08f333373893e514401d
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2019-09-28 12:37:57 +0700

    added mapper

--- src/main/java/com/myapp/pengeluaranku/mapper/BaseMapper.java
+++ src/main/java/com/myapp/pengeluaranku/mapper/BaseMapper.java
@@ -3,8 +3,11 @@ package com.myapp.pengeluaranku.mapper;
 import java.util.List;
 
 public interface BaseMapper<M, V> {
+
+
   V toVO(M model);
   M toModel(V vo);
-  List<V> toListVO(List<M> models);
-  List<M> toListModel(List<V> vos);
+  List<V> toVO(List<M> models);
+  List<M> toModel(List<V> vos);
+
 }
