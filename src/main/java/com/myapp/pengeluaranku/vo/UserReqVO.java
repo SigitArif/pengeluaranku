95d596b782215a7f6699dd4b253c62e2ace10161
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2019-10-02 21:43:56 +0700

    added notblank annotation

--- src/main/java/com/myapp/pengeluaranku/vo/UserReqVO.java
+++ src/main/java/com/myapp/pengeluaranku/vo/UserReqVO.java
@@ -1,12 +1,15 @@
 package com.myapp.pengeluaranku.vo;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class UserReqVO{
-    
+    @NotBlank(message = "Name can't be empty")
     private String name;
     private String address;
+    @NotBlank(message = "Phone can't be empty")
     private String phone;
     private String email;
     public UserReqVO(){};
