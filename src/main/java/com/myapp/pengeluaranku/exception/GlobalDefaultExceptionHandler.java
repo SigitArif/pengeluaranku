abc2d5247d11c6598bee082288c58efbaddb1e49
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2019-10-02 21:44:15 +0700

    added exception handler

--- src/main/java/com/myapp/pengeluaranku/exception/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/myapp/pengeluaranku/exception/GlobalDefaultExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.myapp.pengeluaranku.exception;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,6 +23,29 @@ import com.myapp.pengeluaranku.vo.ResultVO;
 public class GlobalDefaultExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalDefaultExceptionHandler.class);
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<?> notValidHandler(HttpServletRequest req, MethodArgumentNotValidException ex) {
+
+        log.error(String.format("%s : Caught in Global Exception Handler for req: %s",
+                ex.getLocalizedMessage(), req.getRequestURL()));
+
+        String errors;
+        List<String> errorList = new ArrayList<String>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errorList.add(error.getDefaultMessage());
+        }
+
+        errors = errorList.stream().collect(Collectors.joining(", "));
+
+        ResultVO restResponseVO = new ResultVO();
+        restResponseVO.setResults(errors);
+        restResponseVO.setMessage("ERROR");
+        restResponseVO.setStatus(400);
+
+        return new ResponseEntity<>(restResponseVO, HttpStatus.BAD_REQUEST);
+
+    }
 
     @ExceptionHandler(value = {Exception.class, RuntimeException.class })
     public ResponseEntity<?> defaultErrorHandler(HttpServletRequest req, Exception e) {
