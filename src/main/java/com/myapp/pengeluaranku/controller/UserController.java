d1beb7987fd71196b00194a549ca848fb47a93f7
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2019-10-02 22:42:43 +0700

    added edit user

--- src/main/java/com/myapp/pengeluaranku/controller/UserController.java
+++ src/main/java/com/myapp/pengeluaranku/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import com.myapp.pengeluaranku.service.UserService;
 import com.myapp.pengeluaranku.service.impl.UserAddService;
+import com.myapp.pengeluaranku.service.impl.UserEditService;
 import com.myapp.pengeluaranku.util.RestUtil;
 import com.myapp.pengeluaranku.vo.ResultVO;
 import com.myapp.pengeluaranku.vo.UserReqVO;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +22,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value ="pengeluaranku-service/api/v1/user")
 public class UserController{
 @Autowired
-UserAddService userService;
+UserAddService userAddService;
+@Autowired
+UserEditService userEditService;
     @PostMapping(value="add")
     @ResponseBody
     public ResponseEntity<ResultVO> add(@RequestBody @Valid UserReqVO vo){
         ResultVO result = new ResultVO();
-        Object results = userService.add(vo);
+        Object results = userAddService.add(vo);
         result.setMessage("User Added");
         result.setStatus(200);
         result.setResults(results);
         return RestUtil.getJsonResponse(result);
         
+    }
+    @PostMapping(value="edit")
+    @ResponseBody
+    public ResponseEntity<ResultVO> edit(@RequestBody @Valid UserReqVO vo,
+                                         @RequestParam(value = "uuid", required = false) String uuid){
+                                
+        ResultVO result = new ResultVO();
+        Object results = userEditService.edit(uuid, vo);
+        result.setMessage("User Edited");
+        result.setStatus(200);
+        result.setResults(results);
+        return RestUtil.getJsonResponse(result);
+
     }
 
 }
