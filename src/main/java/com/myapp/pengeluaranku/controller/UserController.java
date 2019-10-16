55782757fcfaca7147acedc2711c623cf9d8697f
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2019-10-16 11:29:15 +0700

    added delete controller

--- src/main/java/com/myapp/pengeluaranku/controller/UserController.java
+++ src/main/java/com/myapp/pengeluaranku/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import com.myapp.pengeluaranku.service.UserService;
 import com.myapp.pengeluaranku.service.impl.UserAddService;
+import com.myapp.pengeluaranku.service.impl.UserDeleteService;
 import com.myapp.pengeluaranku.service.impl.UserEditService;
 import com.myapp.pengeluaranku.util.RestUtil;
 import com.myapp.pengeluaranku.vo.ResultVO;
@@ -11,6 +12,7 @@ import com.myapp.pengeluaranku.vo.UserReqVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +23,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value ="pengeluaranku-service/api/v1/user")
 public class UserController{
-@Autowired
+
 UserAddService userAddService;
-@Autowired
 UserEditService userEditService;
+UserDeleteService userDeleteService;
+@Autowired
+public UserController(UserAddService userAddService, UserEditService userEditService, UserDeleteService userDeleteService){
+   this.userAddService = userAddService;
+   this.userEditService = userEditService;
+   this.userDeleteService = userDeleteService;
+}
+
     @PostMapping(value="add")
     @ResponseBody
     public ResponseEntity<ResultVO> add(@RequestBody @Valid UserReqVO vo){
@@ -43,6 +52,18 @@ UserEditService userEditService;
                                 
         ResultVO result = new ResultVO();
         Object results = userEditService.edit(uuid, vo);
+        result.setMessage("User Edited");
+        result.setStatus(200);
+        result.setResults(results);
+        return RestUtil.getJsonResponse(result);
+
+    }
+
+    @DeleteMapping(value="delete")
+    @ResponseBody
+    public ResponseEntity<ResultVO> delete (@RequestParam (value = "uuid", required = false) String uuid){
+        ResultVO result = new ResultVO();
+        Object results = userDeleteService.delete(uuid);
         result.setMessage("User Edited");
         result.setStatus(200);
         result.setResults(results);
