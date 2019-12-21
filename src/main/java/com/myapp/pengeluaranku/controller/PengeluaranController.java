1ae8d5f16a01c41582371a982a4be667085b4172
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2019-12-21 15:11:50 +0700

    modified method delete pengeluaran

--- src/main/java/com/myapp/pengeluaranku/controller/PengeluaranController.java
+++ src/main/java/com/myapp/pengeluaranku/controller/PengeluaranController.java
@@ -12,6 +12,7 @@ import com.myapp.pengeluaranku.vo.ResultVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,7 +52,7 @@ public class PengeluaranController{
         
     }
 
-    @PutMapping(value="/delete")
+    @DeleteMapping(value="/delete")
     public ResponseEntity<ResultVO> delete(@RequestParam(value="uuid") String uuid){
         ResultVO result = new ResultVO();
         String results = pengeluaranService.delete(uuid);
