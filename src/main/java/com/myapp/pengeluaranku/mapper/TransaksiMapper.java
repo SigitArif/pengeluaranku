896e4e8b61b14e589b11a381a4971ed1e7895a4b
Sigit Arif Anggoro <sigit.arifanggoro@ai.astra.co.id>
2020-02-14 06:16:05 +0700

    added name and trxdate converter

--- src/main/java/com/myapp/pengeluaranku/mapper/TransaksiMapper.java
+++ src/main/java/com/myapp/pengeluaranku/mapper/TransaksiMapper.java
@@ -1,5 +1,7 @@
 package com.myapp.pengeluaranku.mapper;
 
+import java.text.SimpleDateFormat;
+
 import com.myapp.pengeluaranku.domain.Pengeluaran;
 import com.myapp.pengeluaranku.domain.Transaksi;
 import com.myapp.pengeluaranku.vo.PengeluaranRequestVO;
@@ -19,6 +21,16 @@ nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
 nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TransaksiMapper extends BaseMapper<Transaksi, TransaksiReqVO, TransaksiResVO>{
 TransaksiMapper INSTANCE = Mappers.getMapper(TransaksiMapper.class);
+
+@Mapping(target = "name", expression = "java(model.getPengeluaran().getName())")
+TransaksiResVO toVO(Transaksi model);
+@AfterMapping
+default void setTrxDate(@MappingTarget TransaksiResVO vo, Transaksi model) {
+    String pattern = "dd/MM/yyyy";
+    SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+    String trxDate = dateFormat.format(model.getTrxDate());
+    vo.setTanggalTransaksi(trxDate);
+}
 
 
 }
